c3dc2542bb06a3ef713395a634c5a4d077e71f1c
gd5097 <gd5097@hanmail.net>
2022-08-01 12:01:49 +0900

    setting

--- front/festival-page-front/src/main.jsx
+++ front/festival-page-front/src/main.jsx
@@ -1,10 +1,11 @@
 import 'normalize.css';
+import 'react-circular-progressbar/dist/styles.css';
 import './styles/style.css';
 import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import MainPage from './pages/main';
+import MainPage from 'pages/main';
 
 const app = document.getElementById('app');
 
--- front/festival-page-front/src/pages/main.jsx
+++ front/festival-page-front/src/pages/main.jsx
@@ -1,17 +1,18 @@
 import { css } from '@emotion/react';
 import React from 'react';
 
-
+import HelloWorld from 'components/HelloWorld';
 
 export default function MainPage() {
     return (
         <div
-        css = {css`
-            background-color: white;
-            width: 500px;
-            height: 500px;
-        `}>
-
+            css={css`
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            `}
+        >
+            <HelloWorld />
         </div>
     );
 }
