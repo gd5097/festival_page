40ee1d19b255b3f865871857580e3e255bfacf13
YongBum <dt10002@naver.com>
2022-08-01 10:15:00 +0900

    Back End Initializr

---
+++ back/back_end_festival/back_end_festival/src/main/java/com/example/back_end_festival/BackEndFestivalApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_end_festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndFestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndFestivalApplication.class, args);
+	}
+
+}
---
+++ back/back_end_festival/back_end_festival/src/test/java/com/example/back_end_festival/BackEndFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_end_festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndFestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
