c18aaf4cf5ef49f9be18b8d7d1ce9bff10d97bec
gd5097 <gd5097@hanmail.net>
2022-08-01 11:03:52 +0900

    프론트 설정

---
+++ front/festival-page-front/src/main.jsx
@@ -0,0 +1,28 @@
+import 'normalize.css';
+import './styles/style.css';
+import { ThemeProvider } from '@emotion/react';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import MainPage from './pages/main';
+
+const app = document.getElementById('app');
+
+const theme = {
+    colors: {
+        white: '#ffffff',
+        black: '#000000',
+        blackText: '#333333',
+        whiteText: '#ffffff',
+        primary1: '#7262A8',
+        primary2: '#3E355C',
+        primary3: '#282436',
+    },
+};
+
+const root = createRoot(app);
+root.render(
+    <ThemeProvider theme={theme}>
+        <MainPage />
+    </ThemeProvider>,
+);
---
+++ front/festival-page-front/src/pages/main.jsx
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+import React from 'react';
+
+
+
+export default function MainPage() {
+    return (
+        <div
+        css = {css`
+            background-color: white;
+            width: 500px;
+            height: 500px;
+        `}>
+
+        </div>
+    );
+}
